08009d53f4e62006f8f8faab7c15203ecc963596
dcarrillo9181 <27480583+dcarrillo9181@users.noreply.github.com>
2022-10-01 12:48:34 -0700

    HomePage Display

--- src/features/projects/projectsSlice.js
+++ src/features/projects/projectsSlice.js
@@ -7,3 +7,7 @@ export const selectAllProjects = () => {
 export const selectProjectById = (id) => {
     return PROJECTS.find((project) => project.id === id);
 };
+
+export const selectFeaturedProject = () => {
+    return PROJECTS.find((project) => project.featured);
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
@@ -15,8 +16,10 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <Provider store={store}>
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>
+      </Provider>
   </React.StrictMode>
 );
